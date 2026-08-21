c3368a7e66eb8707244fe6a1d989dab5fb6d491d
schr-0dinger <kmkailasnath@gmail.com>
2026-08-21 22:54:22 +0530

    Ship radio 1.4, vibrator 1.2 and protobuf for the last failing services

--- device.mk
+++ device.mk
@@ -141,11 +141,14 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.radio@1.2.vendor \
     android.hardware.radio@1.3.vendor \
+    android.hardware.radio@1.4.vendor \
     android.hardware.soundtrigger@2.2.vendor \
     android.hardware.usb@1.0.vendor \
     android.hardware.usb@1.1.vendor \
     android.hardware.vibrator@1.0.vendor \
-    android.hardware.vibrator@1.1.vendor
+    android.hardware.vibrator@1.1.vendor \
+    android.hardware.vibrator@1.2.vendor \
+    libprotobuf-cpp-lite.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
