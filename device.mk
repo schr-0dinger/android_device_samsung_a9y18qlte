2ea4a13a1bded7ab63767447297dc54a8bc03a40
schr-0dinger <kmkailasnath@gmail.com>
2026-08-22 00:19:34 +0530

    Ship the radio config interfaces rild needs

--- device.mk
+++ device.mk
@@ -143,6 +143,9 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.3.vendor \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.soundtrigger@2.2.vendor \
     android.hardware.usb@1.0.vendor \
     android.hardware.usb@1.1.vendor \
